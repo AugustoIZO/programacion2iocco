8ce1d857914af1218e6e378fbb05dc1895bd0144
AugustoIZO <145560759+AugustoIZO@users.noreply.github.com>
2025-04-15 10:02:20 -0300

    Add files via upload

---
+++ html/info.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startups_errors', 1);
+error_reporting(E_ALL);
+
+
+$edad = 20;
+$altura = 1.78;
+$nombre = "augusto";
+$activo = true;
+
+var_dump($edad);
+var_dump($altura);
+var_dump($nombre);
+var_dump($activo);
+
+$a = 10;
+$b = 2;
+
+$multiplicar = $a * $b;
+var_dump($multiplicar);
+?>
